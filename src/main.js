c4746ec789409e357f75cd46a9b87bfa7040f32c
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-10 21:13:36 +0200

    created routes for all projects + added scroll function to router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import HomePage from './containers/HomePage';
 import TripeoPage from './containers/TripeoPage';
+import TempeoPage from './containers/TempeoPage';
+import BingePage from './containers/BingePage';
+import YouthemePage from './containers/YouthemePage';
+import UfcifyPage from './containers/UfcifyPage';
 
 Vue.use(VueRouter)
 
@@ -11,9 +15,21 @@ Vue.config.productionTip = false
 const router = new VueRouter({
   routes: [
     { path: '/', component: HomePage },
-    { path: '/project/tripeo', component: TripeoPage }
+    { path: '/project/tripeo', component: TripeoPage },
+    { path: '/project/bingewatcher', component: BingePage },
+    { path: '/project/youtheme', component: YouthemePage },
+    { path: '/project/ufcify', component: UfcifyPage },
+    { path: '/project/tempeo', component: TempeoPage },
+    { path: '*', component: HomePage }
   ],
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 new Vue({
