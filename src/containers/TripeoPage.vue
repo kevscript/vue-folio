a1b4eddfe6663d1d3109baa7c783405c8a8a4e2a
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-10 20:31:54 +0200

    created TripeoPage container + added specific route

---
+++ src/containers/TripeoPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container">
+
+      <Header prev="tempeo" next="bingewatcher" />
+
+      <Presentation demo="https://tripeo.herokuapp.com/" repo="https://github.com/kevscript/tripeo" name="Tripeo"
+        :skills='["React", "Redux", "styled-components", "Node", "Jest", "Enzyme"]'
+        :content='["Tripeo is a mobile-friendly application that helps you anticipate weather along your trips.", "You create a day-by-day roadmap of your journey, and the app takes care of the rest."]' />
+
+      <Gallery :images="['tripeo/tripeo1.png', 'tripeo/tripeo2.png', 'tripeo/tripeo3.png', 'tripeo/tripeo4.png']" />
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import Header from '../components/Header';
+  import Presentation from '../components/Presentation';
+  import Gallery from '../components/Gallery';
+
+  export default {
+    name: 'TripeoPage',
+    components: {
+      Header,
+      Presentation,
+      Gallery
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../styles/index.scss";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VueRouter from 'vue-router';
-import HomePage from './containers/HomePage.vue'
+import HomePage from './containers/HomePage';
+import TripeoPage from './containers/TripeoPage';
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,8 @@ Vue.config.productionTip = false
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: HomePage }
+    { path: '/', component: HomePage },
+    { path: '/project/tripeo', component: TripeoPage }
   ],
   mode: 'history'
 })
