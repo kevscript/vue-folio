0b12bcb7382f3d48f52dd0c55ce6eb4c51ae3aa9
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-07-10 20:28:44 +0200

    created header component for projects

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="header">
+    <a class="header-logo" href="/">
+      <img src="../assets/logo.svg" alt="">
+    </a>
+    <div class="header-pagination">
+      <a class="pagination-prev" :href="'project/' + prev">
+        <img src="../assets/arrow.svg" alt="previous project">
+      </a>
+      <a class="pagination-next" :href="'project/' + next">
+        <img src="../assets/arrow.svg" alt="next project">
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+      prev: String,
+      next: String
+    }
+  }
+</script>
