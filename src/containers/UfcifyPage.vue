f0bb8846e56d52c1f5005c8ae5145bba045d406b
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-12-31 17:31:52 +0100

    added info about ufc api being down

--- src/containers/UfcifyPage.vue
+++ src/containers/UfcifyPage.vue
@@ -6,7 +6,7 @@
     repo="https://github.com/kevscript/ufcify" 
     name="Ufcify"
     :skills='["React", "Redux", "Node", "Cheerio"]'
-    :content='["Ufcify is an application that uses UFC data to provide you with relevant informations about the organisation and it fighters.", "Its built on an server that scrapes the data from the official api."]'
+    :content='["Ufcify is an application that uses UFC data to provide you with relevant informations about the organisation and it fighters.", "Its built on an server that scrapes the data from the official api.", "This Api is unfortunately down since early 2019."]'
     :images="['ufcify/ufcify.png']"
     date="Dec. 2018"
   />
