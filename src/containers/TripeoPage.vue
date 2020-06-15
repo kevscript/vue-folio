e5d5d02eb9e31882b10c66df5a54680dc44e91b4
Kevin Ostafinski <kevinostafinski@gmail.com>
2020-06-15 18:48:27 +0200

    added more phone images

--- src/containers/TripeoPage.vue
+++ src/containers/TripeoPage.vue
@@ -1,6 +1,6 @@
 <template>
   <ProjectPage :prev="this.prev" :next="this.next" :demo="this.demo" :repo="this.repo" :name="this.name"
-    :skills="this.skills" :content="this.content" :images="this.images" :date="this.date" />
+    :skills="this.skills" :content="this.content" :images="this.images" :date="this.date" :isPhone="this.isPhone" />
 </template>
 
 
@@ -25,11 +25,12 @@
           "You create a day-by-day roadmap of your journey, and the app takes care of the rest."
         ],
         images: [
-          'tripeo/tripeo2.png',
-          'tripeo/tripeo3.png',
-          'tripeo/tripeo4.png'
+          'tripeo/tripeo-phone-1.png',
+          'tripeo/tripeo-phone-2.png',
+          'tripeo/tripeo-phone-3.png'
         ],
-        date: "Jun. 2019"
+        date: "Jun. 2019",
+        isPhone: "true"
       }
     }
   }
--- src/containers/YouthemePage.vue
+++ src/containers/YouthemePage.vue
@@ -1,29 +1,44 @@
 <template>
-  <ProjectPage 
-    prev="gonerank" 
+  <ProjectPage
+    prev="gonerank"
     next="sniplib"
-    demo="https://youtheme.herokuapp.com/" 
-    repo="https://github.com/kevscript/youtheme" 
+    demo="https://youtheme.herokuapp.com/"
+    repo="https://github.com/kevscript/youtheme"
     name="YouTheme"
-    :skills='["React", "Node", "GraphQL", "Apollo", "MongoDB", "styled-components"]'
-    :content='["Youtheme is a youtube subscriptions sorter, allowing you to create your own feeds by custom themes.", "It uses Apollo on the front and back of the application."]'
-    :images="['youtheme/youtheme.jpg']"
+    :skills="[
+      'React',
+      'Node',
+      'GraphQL',
+      'Apollo',
+      'MongoDB',
+      'styled-components',
+    ]"
+    :content="[
+      'Youtheme is a youtube subscriptions sorter, allowing you to create your own feeds by custom themes.',
+      'It uses Apollo on the front and back of the application.',
+    ]"
+    :images="[
+      'youtheme/youtheme-phone-1.png',
+      'youtheme/youtheme-phone-2.png',
+      'youtheme/youtheme-phone-3.png',
+      'youtheme/youtheme-phone-4.png',
+    ]"
     date="Jan. 2020"
+    isPhone="true"
   />
 </template>
 
-
 <script>
-  import ProjectPage from '../components/ProjectPage'
+import ProjectPage from "../components/ProjectPage";
 
-  export default {
-    name: 'YouthemePage',
-    components: {
-      ProjectPage
-    }
-  }
+export default {
+  name: "YouthemePage",
+  components: {
+    ProjectPage,
+  },
+};
 </script>
 
 <style lang="scss">
-  @import "../styles/index.scss";
+@import "../styles/index.scss";
 </style>
